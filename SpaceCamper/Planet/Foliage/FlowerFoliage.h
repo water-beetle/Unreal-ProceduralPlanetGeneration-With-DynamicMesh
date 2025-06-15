7f03d9732ae0efe196e49e6ffd84b94248f3db1e
water-beetle <60806919+water-beetle@users.noreply.github.com>
2025-06-15 23:15:28 +0900

    Randomize clusters per chunk

--- SpaceCamper/Planet/Foliage/FlowerFoliage.h
+++ SpaceCamper/Planet/Foliage/FlowerFoliage.h
@@ -13,8 +13,25 @@ class SPACECAMPER_API UFlowerFoliage : public UPlanetFoliageBase
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this component's properties
-	UFlowerFoliage();
+        // Sets default values for this component's properties
+        UFlowerFoliage();
+
+protected:
+        /** 최소 클러스터 개수 */
+        UPROPERTY(EditAnywhere, Category = Foliage)
+        int32 MinClusters = 2;
+
+        /** 최대 클러스터 개수 */
+        UPROPERTY(EditAnywhere, Category = Foliage)
+        int32 MaxClusters = 4;
+
+        /** 한 클러스터 당 인스턴스 수 */
+        UPROPERTY(EditAnywhere, Category = Foliage)
+        int32 ClusterSize = 10;
+
+        /** 클러스터 확산 정도 (청크 단위 비율) */
+        UPROPERTY(EditAnywhere, Category = Foliage)
+        float ClusterSpread = 0.1f;
 
 protected:
 	// Called when the game starts
